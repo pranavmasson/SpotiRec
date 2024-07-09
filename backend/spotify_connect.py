fe9a825ae0788be578722c1ce473e2a1df83dda7
Pranav Masson <massonpranav5@gmail.com>
2024-07-09 00:30:35 -0400

    bro convo

---
+++ backend/spotify_connect.py
@@ -0,0 +1,15 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+SPOTIPY_CLIENT_ID='80739f8423c94704885edf2f4db60bfc'
+SPOTIPY_CLIENT_SECRET='2b0b53ab4bea406e9f1bb06a58031ea4'
+SPOTIPY_REDIRECT_URI='http://localhost'
+
+scope = "user-library-read"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+results = sp.current_user_saved_tracks()
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " – ", track['name'])
