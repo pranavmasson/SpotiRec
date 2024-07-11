5401b58cc69c76589648d364cd80dc2ae202a741
pranavmasson <massonpranav5@gmail.com>
2024-07-11 00:27:42 -0400

    frontend to login

--- backend/spotify_connect.py
+++
@@ -1,9 +0,0 @@
-import spotipy
-import spotipy.util as util
-from spotipy.oauth2 import SpotifyClientCredentials
-from spotipy.oauth2 import SpotifyOAuth
-cid = 'cd61dc7b3c404b62aae79d5e3c1ef779'
-secret = '2cfe66a686d94dbf926656c3da834236'
-scope = "playlist-modify-public"
-token = util.prompt_for_user_token('USERNAME_TO_AUTHORIZE',scope,client_id=cid,client_secret=secret,redirect_uri='http://localhost:3000/spotirec')
-spotify = spotipy.Spotify(auth=token)  
---
+++ spotify_connect.py
@@ -0,0 +1,54 @@
+import spotipy
+import spotipy.util as util
+from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
+from flask import Flask, render_template, request, redirect, session, url_for
+import os
+
+
+app = Flask(__name__)
+app.secret_key = 'some_secret_key'
+app.config['SESSION_COOKIE_NAME'] = 'spotify-auth-session'
+
+
+cid = 'cd61dc7b3c404b62aae79d5e3c1ef779'
+secret = '2cfe66a686d94dbf926656c3da834236'
+redirect_uri = 'http://localhost:3000/spotirec'
+scope = "playlist-modify-public"
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    auth_url = 'https://accounts.spotify.com/authorize'
+    response_type = 'code'
+    state = ''
+    show_dialog = 'true'
+
+    url = f'{auth_url}?response_type={response_type}&client_id={cid}&scope={scope}&redirect_uri={redirect_uri}&state={state}&show_dialog={show_dialog}'
+    return redirect(url)
+
+
+@app.route('/spotirec')
+def spotirec():
+    code = request.args.get('code')
+    if code:
+        token_info = util.prompt_for_user_token(
+            'USERNAME_TO_AUTHORIZE',
+            scope,
+            client_id=cid,
+            client_secret=secret,
+            redirect_uri=redirect_uri
+        )
+        spotify = spotipy.Spotify(auth=token_info['access_token'])
+        session['token_info'] = token_info
+        return render_template('success.html')
+    else:
+        return 'Failed to get authentication code'
+    
+if __name__ == '__main__':
+    app.run(port=3000) 
