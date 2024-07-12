e413439b563d08bdaad809d322854e90050886c3
pranavmasson <massonpranav5@gmail.com>
2024-07-11 21:53:09 -0400

    fixed spotify login to work with anyone that signs in

--- spotify_connect.py
+++ spotify_connect.py
@@ -18,6 +18,18 @@ scope = "user-read-recently-played"
 
 sp_oauth = SpotifyOAuth(client_id=cid, client_secret=secret, redirect_uri=redirect_uri, scope=scope)
 
+def get_token():
+    token_info = session.get('token_info', None)
+    if not token_info:
+        return None
+
+    # Check if token has expired
+    if sp_oauth.is_token_expired(token_info):
+        token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
+        session['token_info'] = token_info
+
+    return token_info
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -27,26 +39,27 @@ def login():
     auth_url = sp_oauth.get_authorize_url()
     return redirect(auth_url)
 
+@app.route('/callback')
+def callback():
+    session.clear()
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
+
+    session['token_info'] = token_info
+    return redirect(url_for('spotirec'))
+
 @app.route('/spotirec')
 def spotirec():
-    code = request.args.get('code')
-    if code:
-        token_info = sp_oauth.get_access_token(code)
-        if token_info:
-            token = token_info['access_token']
-            spotify = spotipy.Spotify(auth=token)
-            session['token_info'] = token_info
+    token_info = get_token()
+    if not token_info:
+        return redirect('/')
 
-            #tracks = spotify.current_user_recently_played(limit=50, after=None, before=None)
-            tracks = ""
-            user_name = spotify.current_user()
-            user_name = user_name['display_name']
+    sp = spotipy.Spotify(auth=token_info['access_token'])
+    results = sp.current_user_recently_played()
+    user_name = sp.current_user()['display_name']
+    tracks = results['items']
 
-            return render_template('dashboard.html', user_name=user_name, tracks=tracks)
-        else:
-            return 'Failed to get the token'
-    else:
-        return 'Failed to get authentication code'
+    return render_template('dashboard.html', user_name=user_name, tracks=tracks)
 
 if __name__ == '__main__':
     app.run(port=3000)
